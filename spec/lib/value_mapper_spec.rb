833d90c185322656cd4d118cf05eb2840357e587
David <vjdavid621@gmail.com>
2020-08-31 14:51:31 -0700

    adding spec of ValueMapper class

--- spec/lib/value_mapper_spec.rb
+++ spec/lib/value_mapper_spec.rb
@@ -13,12 +13,10 @@ describe ValueMapper do
     end
 
     context 'with status code IT' do
-      ValueMapper::MAPPING_VALUES.each do |key, value|
-        let(:status_codes) { { status_code: key }.to_json }
+      let(:status_codes) { { status_code: 'IT' }.to_json }
 
-        it 'should convert value to on transit' do
-          expect(map_values).to include(status: value)
-        end
+      it 'should convert value to on transit' do
+        expect(map_values).to include(status: 'ON_TRANSIT')
       end
     end
 
@@ -35,6 +33,14 @@ describe ValueMapper do
 
       it 'should convert value to exception' do
         expect(map_values).to include(status: 'EXCEPTION')
+      end
+    end
+
+    context 'with undefined status code' do
+      let(:status_codes) { { status_code: 'JJ' }.to_json }
+
+      it 'should convert anything' do
+        expect(map_values).not_to respond_to(:status)
       end
     end
   end
